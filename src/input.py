348d7087d2852c4f90fbdd34bac6a388f2220ef5
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-09 11:43:27 -0600

    Update

--- src/input.py
+++ src/input.py
@@ -1,2 +1,7 @@
-name = input("What's your name?")
+#Ask the user for their name
+#Then, remove possible whitespaces from the given input (strip function)
+#and then, capitalize user's name and a possible second characters (title function)
+name = input("What's your name?").strip().title()
+
+#Say hello
 print("Hello " + name + "!")
