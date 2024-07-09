050965e0ea89fe5dbad50c175a466fe363f3c88c
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-09 11:58:59 -0600

    Update

---
+++ src/calculator.py
@@ -0,0 +1,10 @@
+#Asking for the user's x and y value
+x = input("Insert x value ")
+y = input("Insert y value ")
+
+#Sum the values (we have to convert them to
+#int to use the + character properly)
+z = int(x) + int(y)
+
+#Print the final result
+print("The sum of", x, "+", y, "=", z)
