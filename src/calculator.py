4c697db37186696af359a48f546d3533c4f28000
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-11 23:22:30 -0600

    Update

--- src/calculator.py
+++ src/calculator.py
@@ -6,8 +6,13 @@ y = input("Insert y value ")
 #int to use the + character properly)
 z = float(x) + float(y)
 
-#Round the result to the nearest int
-z = round(z)
+#Round the result to the nearest hundredth
+z = round(z, 2)
 
 #Print the final result
-print("The sum of", x, "+", y, "=", z)
+print("The sum of", x, "+", y, "=", f"{z:,}")
+
+#f"{z}" is just another way of calling the funciton
+#in this case to print. While the ':,' part will just
+#add the needed commas according to the need or length
+#of the number
