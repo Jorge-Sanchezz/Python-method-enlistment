98d537170d93728dcaf0b2430e584bf52cfa6d05
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-12 00:17:16 -0600

    Update

---
+++ src/6-conditionals.py
@@ -0,0 +1,9 @@
+x = input("Insert x value ")
+y = input("Insert y value ")
+
+if x < y:
+    print("x is less than y")
+elif x < y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
