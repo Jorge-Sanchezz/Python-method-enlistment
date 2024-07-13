b3f182c04bcd7cb81b05f93b860db9ffb1f23493
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-13 17:29:03 -0600

    Update

---
+++ src/12-dictionary.py
@@ -0,0 +1,16 @@
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor", 
+    "Ron": "Gryffindor", 
+    "Draco": "Slytherin"
+}
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for i in students:
+    print(i, students[i], sep=", ")
