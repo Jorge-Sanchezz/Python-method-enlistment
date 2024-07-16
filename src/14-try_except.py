4551078cfe73d34f42f24d9de58f106edc0b51e0
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-15 23:41:18 -0600

    Update

--- src/14-try_except.py
+++ src/14-try_except.py
@@ -1,7 +1,9 @@
 try:
     x = int(input("What's number x? "))
-    print(f"x is {x}")
     
 #Catch an value error if founded one
 except ValueError:
     print("Your input is not an integer")
+
+else:
+    print(f"x is {x}")
