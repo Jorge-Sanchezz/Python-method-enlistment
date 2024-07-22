f5cb810f74929517a03ff3f21edecc5dd1d0d14c
Jorge-Sanchezz <jsanchezencinas0804@gmail.com>
2024-07-22 00:53:48 -0600

    Calculator test update

--- src/test_calculator_22.py
+++ src/test_calculator_22.py
@@ -2,9 +2,13 @@ from tested_calculator_21 import square
 
 #Run with 'pytest' tool after installing it writing 'pip install pytest' on your 
 #terminal (to run it, just write 'pytest test_calculator_22.py' in this case)
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
+    
+def test_negative():
     assert square(-2) == 4
     assert square(-3) == 9
+
+def test_zero():
     assert square(0) == 0
